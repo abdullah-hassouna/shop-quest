47095d3e5db99657eb41c94eff0ac1ee97f4daa4
abdullah-hassouna <abood211957@outlook.com>
2025-08-25 22:56:58 +0300

    fix product image case (no image)

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -34,8 +34,8 @@ export default function ProductCard({ product }: { product: GetProductDataRespon
     <div className="group relative">
       <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
         <img
-          src={product.imagesId ? product.imagesId[0].url : ""}
-          alt={product.imagesId ? product.imagesId[0].alt : ""}
+          src={product.imagesId.length ? product.imagesId[0].url : ""}
+          alt={product.imagesId.length ? product.imagesId[0].alt : ""}
           className="w-full h-full object-center object-cover lg:w-full lg:h-full"
         />
       </div>
